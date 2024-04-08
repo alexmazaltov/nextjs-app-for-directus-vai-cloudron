988d26d1a7608b7ae5638d908de96756f94b35bc
Alex.Mazaltov <1605-alex.mazaltov@users.noreply.drupalcode.org>
2024-04-08 21:06:01 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // ... rest of the configuration.
+//   output: "standalone",
+// };
+
+// export default nextConfig;
+
+// next.config.js
+module.exports = {
+  // ... rest of the configuration.
+  output: "standalone",
+};

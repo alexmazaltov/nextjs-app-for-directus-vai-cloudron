d21225355d2444cc1a95b4906e195ce2afce8fb5
Alex.Mazaltov <1605-alex.mazaltov@users.noreply.drupalcode.org>
2024-04-27 23:07:10 +0300

    Updated OpenGraph metadata

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,31 @@
 export const metadata = {
-  title: 'NEW GENERATION INSTITUTES',
-  description: 'Invest.Auction',
-}
+  metadataBase: new URL("https://app.cloud.alefinvest.xyz"),
+  title: 'NGI | New Generation Institutes',
+  description: 'Invest in Auction.',
+  keywords: [
+    "invest in auction",
+    "referral program",
+    "make money online",
+    "social media",
+  ],
+  applicationName: "Invest-in-Auction",
+  authors: [{ name: "Alex Mazaltov" }],
+  category: "Affiliate marketing",
+  openGraph: {
+    type: "website",
+    countryName: "Ukraine",
+    images: ["https://app.cloud.alefinvest.xyz/ngi-logo.jpeg"],
+    siteName: "Invest.Auction",
+    title: "NGI | New Generation Institutes",
+    description: "Invest in Auction.",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://app.cloud.alefinvest.xyz/ngi-logo.jpeg"],
+    title: "NGI | New Generation Institutes",
+    description: "Invest in Auction. Get more money from your traffic.",
+  },
+};
 
 export default function RootLayout({ children }) {
  return (
--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,21 @@
 import directus from '../lib/directus';
 import { readItems } from '@directus/sdk';
+import Image from 'next/image'
+
 
 async function getGlobals() {
 	return directus.request(readItems('global'));
-}
+} 
 
 export default async function HomePage() {
 	const global = await getGlobals();
 	return (
 		<div>
-			<h1>{global.title}</h1>
-			<p>{global.description}</p>
+			<center>
+				<Image src={`/ngi-logo.jpeg`} alt="NEW GENERATION INSTITUTES" width="320" height="320" />
+				<h1>{global.title}</h1>
+				<p>{global.description}</p>
+			</center>
 		</div>
 	);
 }
